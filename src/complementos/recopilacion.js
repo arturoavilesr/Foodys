89845e0ac1d5d83798ab73bceb56e8cbeb3f5b55
arturoavilesr <145037428+arturoavilesr@users.noreply.github.com>
2023-10-23 20:44:48 -0600

    agregar sistema

---
+++ src/complementos/recopilacion.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export const recopilacion = () => {
+    
+    return (
+        <div>
+            <form>
+                <input
+            type="text"
+            name="search"
+            autoComplete="=off"
+         
+            ></input>
+            <button type="submit">ingredientes </button>
+
+            </form>
+
+
+        </div>
+    );
+};
+
+

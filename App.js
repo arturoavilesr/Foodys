c8470f89cc72e59ea87c3803c2d88735355006dc
arturoavilesr <145037428+arturoavilesr@users.noreply.github.com>
2023-10-23 20:36:15 -0600

    SRC

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { recopilacion } from './complementos/recopilacion';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         probando
+        </p>
+
+<recopilacion/>
+      </header>
+    </div>
+  );
+}
+
+export default App;

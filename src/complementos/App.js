16e234b362657da813ac012414dab3e31d893846
arturoavilesr <145037428+arturoavilesr@users.noreply.github.com>
2023-10-24 22:51:09 -0600

    Create App.js
    Se crea componente app y agrega codigo

---
+++ src/complementos/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import IngredientForm from './IngredientForm';
+
+function App() {
+  return (
+    <div className="App">
+      <IngredientForm />
+    </div>
+  );
+}
+
+export default App;

e3d982157266d6ea475e72207f6f0270228275af
arturoavilesr <145037428+arturoavilesr@users.noreply.github.com>
2023-10-26 11:03:44 -0600

    Create app.cs
    lo que faltaba en el codigo de app

---
+++ src/recetas/app.cs
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RecipeForm from './RecipeForm';
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeForm />
+    </div>
+  );
+}
+
+export default App;

77af9ebff859f640102e38c9a787b60952c1dae8
arturoavilesr <145037428+arturoavilesr@users.noreply.github.com>
2023-11-02 22:06:48 -0600

    Update Ingredientform.js
    se modifica para mejor diseño

--- src/complementos/Ingredientform.js
+++ src/complementos/Ingredientform.js
@@ -1,4 +1,25 @@
-function IngredientForm(){
+import React, { Component } from 'react';
+
+class IngredientForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredient: ''
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ ingredient: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Ingrediente ingresado:', this.state.ingredient);
+    this.setState({ ingredient: '' });
+  }
+
+  render() {
+    return (
       <div>
         <h2>Formulario de Ingredientes</h2>
         <form onSubmit={this.handleSubmit}>
@@ -9,7 +30,8 @@ function IngredientForm(){
           <button type="submit">Agregar Ingrediente</button>
         </form>
       </div>
+    );
+  }
 }
+
 export default IngredientForm;
-
-

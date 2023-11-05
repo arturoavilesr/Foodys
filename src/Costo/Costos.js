6dfd872acb9c95c9276e6b21697c37adce6fa6a1
arturoavilesr <145037428+arturoavilesr@users.noreply.github.com>
2023-11-04 23:15:06 -0600

    Update Costos.js
    se ingresa la primera parte del codigo

--- src/Costo/Costos.js
+++ src/Costo/Costos.js
@@ -1,1 +1,31 @@
+class IngredientForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      cost: '',
+      unit: ''
+    };
+  }
 
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Ingrediente ingresado:', this.state);
+    // Aquí puedes realizar acciones adicionales con el ingrediente
+    this.setState({ name: '', cost: '', unit: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Formulario de Ingredientes</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Nombre del Ingrediente:
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+          </label>
+          <label>
